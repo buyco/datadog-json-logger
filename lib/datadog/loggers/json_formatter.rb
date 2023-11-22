7b0b9e174e70872c7bf58e914502a7e70e52b020
Eth3rnit3 <eth3rnit3@gmail.com>
2023-11-22 23:01:25 +0100

    feat: improve formatter and specs

--- lib/datadog/loggers/json_formatter.rb
+++ lib/datadog/loggers/json_formatter.rb
@@ -8,35 +8,35 @@ module Datadog
   module Loggers
     class JSONFormatter < Logger::Formatter
       def self.call(severity, datetime, progname, msg)
-        log_hash = {
+        log_hash = base_log_hash(severity, datetime, progname)
+        formatter = formatter_for(msg)
+        formatter.format(log_hash, msg)
+
+        yield(log_hash) if block_given?
+
+        "#{log_hash.to_json}\r\n"
+      end
+
+      def self.base_log_hash(severity, datetime, progname)
+        {
           dd: correlation_hash,
           timestamp: datetime.to_s,
           severity: severity.ljust(5).to_s,
           progname: progname.to_s
         }
+      end
 
-        if msg.is_a?(Hash) && msg.key?(:request)
-          log_hash.merge!(msg)
-        elsif msg.is_a?(Exception)
-          log_hash.merge!(
-            exception: msg,
-            exception_message: msg.message,
-            exception_backtrace: msg.backtrace
-          )
-        elsif msg.instance_of?(String)
-          log_hash[:message] = msg.dup.force_encoding("utf-8")
-        else
-          log_hash[:message] = msg.to_s
+      def self.formatter_for(msg)
+        case msg
+        when Hash then HashFormatter
+        when Exception then ExceptionFormatter
+        when String then StringFormatter
+        else DefaultFormatter
         end
-
-        yield(log_hash) if block_given?
-
-        log_hash.to_json + "\r\n" # rubocop:disable Style/StringConcatenation
       end
 
       def self.correlation_hash
         correlation = Datadog::Tracing.correlation
-
         {
           trace_id: correlation.trace_id&.to_s,
           span_id: correlation.span_id&.to_s,
@@ -44,6 +44,34 @@ module Datadog
           service: correlation.service&.to_s,
           version: correlation.version&.to_s
         }
+      end
+
+      class HashFormatter
+        def self.format(log_hash, msg)
+          log_hash.merge!(msg)
+        end
+      end
+
+      class ExceptionFormatter
+        def self.format(log_hash, exception)
+          log_hash.merge!(
+            exception: exception,
+            exception_message: exception.message,
+            exception_backtrace: exception.backtrace
+          )
+        end
+      end
+
+      class StringFormatter
+        def self.format(log_hash, msg)
+          log_hash[:message] = msg.dup.force_encoding("utf-8")
+        end
+      end
+
+      class DefaultFormatter
+        def self.format(log_hash, msg)
+          log_hash[:message] = msg.to_s
+        end
       end
     end
   end
--- spec/datadog/loggers/formatter_spec.rb
+++ spec/datadog/loggers/formatter_spec.rb
@@ -3,7 +3,7 @@
 require "spec_helper"
 require "datadog/loggers/json_formatter"
 
-class CustomerFormatter < Datadog::Loggers::JSONFormatter
+class CustomFormatter < Datadog::Loggers::JSONFormatter
   def self.call(severity, datetime, progname, msg)
     super do |log_hash|
       log_hash[:my_custom_key] = "my_value"
@@ -12,21 +12,62 @@ class CustomerFormatter < Datadog::Loggers::JSONFormatter
 end
 
 RSpec.describe Datadog::Loggers::JSONFormatter do
+  let(:datetime) { Time.now }
+  let(:severity) { "INFO" }
+  let(:progname) { "progname" }
   let(:json) { JSON.parse(result) }
 
   describe "#call" do
-    let(:result) { described_class.call("INFO", Time.now, "progname", "Test message") }
+    context "when message is a string" do
+      let(:result) { described_class.call(severity, datetime, progname, "Test message") }
 
-    it { expect { json }.not_to raise_error }
-    it { expect(json).to include("message", "dd", "progname", "severity", "timestamp") }
-    it { expect(json["dd"]).to include("env", "service", "span_id", "trace_id", "version") }
+      it { expect { json }.not_to raise_error }
+      it { expect(json).to include("message" => "Test message") }
+
+      it { expect(json).to include("dd", "progname", "severity", "timestamp") }
+      it { expect(json["dd"]).to include("env", "service", "span_id", "trace_id", "version") }
+    end
+
+    context "when message is a hash" do
+      let(:message_hash) { { key1: "value1", key2: "value2" } }
+      let(:result) { described_class.call(severity, datetime, progname, message_hash) }
+
+      it { expect(json).to include("key1" => "value1", "key2" => "value2") }
+
+      it { expect(json).to include("dd", "progname", "severity", "timestamp") }
+      it { expect(json["dd"]).to include("env", "service", "span_id", "trace_id", "version") }
+    end
+
+    context "when message is an exception" do
+      let(:exception) { StandardError.new("error message") }
+      let(:result) { described_class.call(severity, datetime, progname, exception) }
+
+      it { expect(json).to include("exception_message" => "error message") }
+      it { expect(json).to include("exception_backtrace") }
+
+      it { expect(json).to include("dd", "progname", "severity", "timestamp") }
+      it { expect(json["dd"]).to include("env", "service", "span_id", "trace_id", "version") }
+    end
+
+    context "when message is a generic object" do
+      let(:object) { Object.new }
+      let(:result) { described_class.call(severity, datetime, progname, object) }
+
+      it { expect(json).to include("message" => object.to_s) }
+
+      it { expect(json).to include("dd", "progname", "severity", "timestamp") }
+      it { expect(json["dd"]).to include("env", "service", "span_id", "trace_id", "version") }
+    end
   end
 
-  describe "class ineritance" do
-    context "when add custom key to json formatter" do
-      let(:result) { CustomerFormatter.call("INFO", Time.now, "progname", "Test message") }
+  describe "class inheritance" do
+    context "when adding custom key to json formatter" do
+      let(:result) { CustomFormatter.call(severity, datetime, progname, "Test message") }
 
-      it { expect(json).to include("my_custom_key") }
+      it { expect(json).to include("my_custom_key" => "my_value") }
+
+      it { expect(json).to include("dd", "progname", "severity", "timestamp") }
+      it { expect(json["dd"]).to include("env", "service", "span_id", "trace_id", "version") }
     end
   end
 end
