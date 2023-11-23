d8f61c6b392f9c504cea9c6e302051f3c719a5ad
Eth3rnit3 <eth3rnit3@gmail.com>
2023-11-23 14:29:13 +0100

    try version

--- lib/datadog/loggers/version.rb
+++ lib/datadog/loggers/version.rb
@@ -2,6 +2,6 @@
 
 module Datadog
   module Loggers
-    VERSION = "0.1.1-test"
+    VERSION = "0.1.1-test-1"
   end
 end
--- nexus_release.sh
+++ nexus_release.sh
@@ -7,7 +7,4 @@ if [ -z "$gem_file" ]; then
     exit 1
 fi
 
-echo "URL Nexus: $NEXUS_DEPLOY_URL"
-echo "Credentials Nexus: $NEXUS_CREDENTIALS"
-
 gem nexus --url $NEXUS_DEPLOY_URL --credential $NEXUS_CREDENTIALS $gem_file
