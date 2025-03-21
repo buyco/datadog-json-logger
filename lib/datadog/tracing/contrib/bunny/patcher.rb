df7c36b06e3ac98fd2532227fc12e9bfd416e8b9
Eth3rnit3 <eth3rnit3@gmail.com>
2025-03-21 10:28:14 +0100

    add consumer patcher

--- lib/datadog/tracing/contrib/bunny/patcher.rb
+++ lib/datadog/tracing/contrib/bunny/patcher.rb
@@ -21,71 +21,84 @@ module Datadog
             ::Bunny::Channel.prepend(ChannelPatch)
             ::Bunny::Exchange.prepend(ExchangePatch)
             ::Bunny::Queue.prepend(QueuePatch)
+            ::Bunny::Consumer.prepend(ConsumerPatch)
           end
 
           # Patch for Bunny::Channel
           module ChannelPatch
             def basic_consume(queue, consumer_tag = generate_consumer_tag, no_ack = false, exclusive = false,
                               arguments = nil, &block)
-              res = nil
               config = Datadog.configuration.tracing[:bunny]
               Datadog::Tracing.trace(Ext::SPAN_CONSUME, service: config.service_name) do |span|
                 span.type = Datadog::Tracing::Metadata::Ext::AppTypes::TYPE_WORKER
                 span.set_tag(Datadog::Tracing::Metadata::Ext::TAG_COMPONENT, Ext::APP)
                 span.set_tag(Ext::SPAN_CHANNEL_ID, id)
 
-                res = super
+                super
               end
-
-              res
             end
 
             def basic_publish(payload, exchange, routing_key, opts = {})
-              res = nil
               config = Datadog.configuration.tracing[:bunny]
               Datadog::Tracing.trace(Ext::SPAN_BASIC_PUBLISH, service: config.service_name) do |span|
                 span.type = Datadog::Tracing::Metadata::Ext::AppTypes::TYPE_WORKER
                 span.set_tag(Datadog::Tracing::Metadata::Ext::TAG_COMPONENT, Ext::APP)
                 span.set_tag(Ext::SPAN_CHANNEL_ID, id)
 
-                res = super
-              end
+                if (trace_digest = Datadog::Tracing.active_trace&.to_digest)
+                  opts[:headers] ||= {}
+                  hash = JSON.parse(trace_digest.to_json)
+                  opts[:headers].merge!(hash)
+                end
 
-              res
+                super
+              end
             end
           end
 
           # Patch for Bunny::Exchange
           module ExchangePatch
             def publish(payload, opts = {})
-              res = nil
               config = Datadog.configuration.tracing[:bunny]
               Datadog::Tracing.trace(Ext::SPAN_EXCHANGE_PUBLISH, service: config.service_name) do |span|
                 span.type = Datadog::Tracing::Metadata::Ext::AppTypes::TYPE_WORKER
                 span.set_tag(Datadog::Tracing::Metadata::Ext::TAG_COMPONENT, Ext::APP)
                 span.set_tag(Ext::SPAN_EXCHANGE_NAME, name)
 
-                res = super
+                if (trace_digest = Datadog::Tracing.active_trace&.to_digest)
+                  opts[:headers] ||= {}
+                  hash = JSON.parse(trace_digest.to_json)
+                  opts[:headers].merge!(hash)
+                end
+                super
               end
-
-              res
             end
           end
 
           # Patch for Bunny::Queue
           module QueuePatch
             def pop(opts = { manual_ack: false }, &block)
-              res = nil
               config = Datadog.configuration.tracing[:bunny]
               Datadog::Tracing.trace(Ext::SPAN_QUEUE_POP, service: config.service_name) do |span|
                 span.type = Datadog::Tracing::Metadata::Ext::AppTypes::TYPE_WORKER
                 span.set_tag(Datadog::Tracing::Metadata::Ext::TAG_COMPONENT, Ext::APP)
                 span.set_tag(Ext::SPAN_QUEUE_NAME, name)
 
-                res = super
+                super
               end
+            end
+          end
 
-              res
+          # Patch for Bunny::Consumer
+          module ConsumerPatch
+            def on_delivery(&block)
+              config = Datadog.configuration.tracing[:bunny]
+              Datadog::Tracing.trace(Ext::SPAN_CONSUME, service: config.service_name) do |span|
+                span.type = Datadog::Tracing::Metadata::Ext::AppTypes::TYPE_WORKER
+                span.set_tag(Datadog::Tracing::Metadata::Ext::TAG_COMPONENT, Ext::APP)
+
+                super
+              end
             end
           end
         end
