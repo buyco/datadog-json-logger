40518157def799d411d29c16bad2b789f8c70740
Eth3rnit3 <eth3rnit3@gmail.com>
2025-03-21 16:00:32 +0100

    add specs

--- lib/datadog/tracing/contrib/bunny/utils.rb
+++ lib/datadog/tracing/contrib/bunny/utils.rb
@@ -11,17 +11,29 @@ module Datadog
           # @return [void]
           def continue_trace!(metadata)
             serializes_trace_digest = if metadata.is_a?(Hash)
-              metadata.deep_symbolize_keys
-            elsif metadata.is_a?(String)
-              JSON.parse(metadata, symbolize_names: true)
-            else
-              return
-            end
+                                        # Convertit les clés en symboles de manière récursive
+                                        symbolize_keys(metadata)
+                                      elsif metadata.is_a?(String)
+                                        JSON.parse(metadata, symbolize_names: true)
+                                      else
+                                        return
+                                      end
 
             trace_digest = Datadog::Tracing::TraceDigest.new(**serializes_trace_digest)
             return unless trace_digest
 
             Datadog::Tracing.continue_trace!(trace_digest)
+          end
+
+          # Méthode pour convertir récursivement les clés d'un hash en symboles
+          # @param hash [Hash] Le hash à convertir
+          # @return [Hash] Le hash avec des clés symboliques
+          def symbolize_keys(hash)
+            hash.each_with_object({}) do |(key, value), result|
+              new_key = key.is_a?(String) ? key.to_sym : key
+              new_value = value.is_a?(Hash) ? symbolize_keys(value) : value
+              result[new_key] = new_value
+            end
           end
         end
       end
